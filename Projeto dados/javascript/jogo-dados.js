8556cf8feec96418933174eaaab5213d71073b97
Jean Gomes <jeansantos@uni9.edu.br>
2020-06-19 11:02:15 -0300

    projeto

---
+++ Projeto dados/javascript/jogo-dados.js
@@ -0,0 +1,9 @@
+function(btnEnvia){
+	var nome;
+	var nick;
+	var idade;
+	/*document.querySelector('#btnEnvia').addEventListener('click', () => {
+		window.location.href = 'jogo-dados.html';	
+	});*/
+	alert('Funcionando');
+}
